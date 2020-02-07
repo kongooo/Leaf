364b55d3e69266c725bf4cfa398303d82d95c569
kongooo <1184701824@qq.com>
2020-02-07 12:01:26 +0800

    disappear

--- JS/loading.js
+++ JS/loading.js
@@ -1,3 +1,10 @@
+let anima = document.querySelector('.load-anima');
+
 window.onload = function() {
-    document.querySelector('.load-anima').style.display = "none";
+    anima.classList.add('anima-opacity');
+    setTimeout("dis()", 1000);
+}
+
+function dis() {
+    anima.style.display = "none";
 }
