db3114463c6688eef5c1e7a52a89f3fa33852c54
kongooo <1184701824@qq.com>
2020-02-07 17:48:48 +0800

    easter egg

--- JS/loading.js
+++ JS/loading.js
@@ -1,10 +1,39 @@
-let anima = document.querySelector('.load-anima');
+let anima = document.querySelector('.load-anima'),
+    temp_sun = document.querySelector(".sun-canvas"),
+    cl_child = document.querySelector('#child');
 
 window.onload = function() {
-    anima.classList.add('anima-opacity');
-    setTimeout("dis()", 1000);
+    disappear();
 }
 
-function dis() {
+function SetNone() {
     anima.style.display = "none";
 }
+
+function SetFlex() {
+    anima.style.display = "flex";
+}
+
+function disappear() {
+    if (!anima.classList.contains('anima-opacity'))
+        anima.classList.add('anima-opacity');
+    if (anima.classList.contains('anima-show'))
+        anima.classList.remove('anima-show');
+    setTimeout("SetNone()", 1000);
+}
+
+function show() {
+    SetFlex();
+    if (anima.classList.contains('anima-opacity'))
+        anima.classList.remove('anima-opacity');
+    if (!anima.classList.contains('anima-show'))
+        anima.classList.add('anima-show');
+}
+
+temp_sun.addEventListener('click', event => {
+    show();
+})
+
+cl_child.addEventListener('click', event => {
+    disappear();
+})
